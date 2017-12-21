346f537af293cadd42a7a51fe93004a07222c5a8
Caique Ferreira <caiquef.ms@gmail.com>
2017-12-21 14:56:26 -0300

    Quarto commit

--- conta.py
+++ conta.py
@@ -1,16 +1,20 @@
 class Conta:
     def __init__(self,numero,titular,saldo,limite = 1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
 
     def extrato(self):
-        print("Saldo {0} do titular {1}".format(self.saldo,self.titular))
+        print("Saldo {0} do titular {1}".format(self.__saldo,self.__titular))
 
     def deposita(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self,valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self,valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)
